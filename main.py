23fcd43bef79eef4974e665b83d398b5c9953d13
RalatPC <ralatrado@gmail.com>
2022-06-08 14:22:25 +0100

    main python file and blank readme file

---
+++ main.py
@@ -0,0 +1,65 @@
+# import random module
+import random
+
+print("Welcome to Our Rock-Paper-Scissors CLI game!!")
+
+start_game = True
+
+while start_game:
+    # Ask for player input
+    
+
+    player_input = str(input("Choose your character: \nEnter 'R' for rock \nEnter 'P' for paper \nEnter 'S' for scissors\nEnter 'Z' to close game.\n"))
+    # except:
+    #     print("You did not enter a string")
+    #     continue
+    if not player_input.isalpha():
+            print("This is not an alphabet")
+            continue
+        
+
+    # convert player input to uppercase for uniformity
+    player_choice = player_input.upper()
+
+    if player_choice == "Z":
+        print("Closed...")
+        break
+
+
+    # list for computer options
+    computer_options = ["R", "P", "S"]
+
+    # use inbuilt choice funtion from inbuilt python random module.
+    computer_choice = random.choice(computer_options)
+
+    print("You chose ", player_choice, " computer chose ", computer_choice)
+
+    if player_choice == computer_choice:
+        print("Both player chose ", player_choice, "It's a tie!")
+    elif player_choice == "R":
+        if computer_choice == "S":
+            print("Rock smashes scissors! You win!")
+        else: 
+            print("Paper covers rock! You lose.")
+    elif player_choice == "P":
+        if computer_choice == "R":
+            print("Paper covers rock! You win!")
+        else: 
+            print("Scissors cuts paper! You lose.")
+
+    elif player_choice == "S":
+        if computer_choice == "P":
+            print("Scissors cuts paper! You win!")
+        else:
+            print("Rock smashes scissors! You lose.")
+    else:
+        print("Oops, ", player_choice, " is an Invalid input....Let's try that again")
+    
+    
+    restart = input("Will you like to play again?\nPress 'y' for yes or 'n' for no.")
+    # Restart program
+    if restart == "y":
+        continue
+    else:
+        print("Thank you for playing.")
+        break
